8f62900da80a8a78470e6a1ed97949e70443439c
seongchanmi <seongchanmi@gmail.com>
2025-11-03 16:35:52 +0900

    팀플

---
+++ Rotten-Tangerines/src/main/java/com/example/Rotten/Tangerines/RottenTangerinesApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rotten.Tangerines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenTangerinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenTangerinesApplication.class, args);
+	}
+
+}
---
+++ Rotten-Tangerines/src/test/java/com/example/Rotten/Tangerines/RottenTangerinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rotten.Tangerines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RottenTangerinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
