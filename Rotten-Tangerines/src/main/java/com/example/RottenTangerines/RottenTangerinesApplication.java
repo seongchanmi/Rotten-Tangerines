9040bbbec1b258498db7c79f92eea4756023cb05
seongchanmi <seongchanmi@gmail.com>
2025-11-04 16:58:55 +0900

    인텔리제이 다시

--- Rotten-Tangerines/src/main/java/com/example/RottenTangerines/RottenTangerinesApplication.java
+++ Rotten-Tangerines/src/main/java/com/example/RottenTangerines/RottenTangerinesApplication.java
@@ -1,4 +1,4 @@
-package com.example.Rotten.Tangerines;
+package com.example.RottenTangerines;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ RottenTangerines/src/main/java/com/example/RottenTangerines/RottenTangerinesApplication.java
@@ -0,0 +1,13 @@
+package com.example.RottenTangerines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RottenTangerinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RottenTangerinesApplication.class, args);
+	}
+
+}
